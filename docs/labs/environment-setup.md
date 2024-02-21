a4d6a27cba324b557a9b1475cb73d042f1f2185e
Scott Root <scottroot2@gmail.com>
2024-02-21 18:02:32 -0500

    Environment setup script and doc.

---
+++ docs/labs/environment-setup.md
@@ -0,0 +1,46 @@
+# Environment Setup
+
+## Local Environment Setup
+
+To develop logic apps locally you need to install the following tools:
+# Environment Setup
+
+## Local Environment Setup
+
+To develop logic apps locally, you need to install the following tools:
+
+- [Azure CLI](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli)
+- [Azure Developer CLI](https://docs.microsoft.com/en-us/azure/developer/azure-cli)
+- [Azure Function Core Tools](https://docs.microsoft.com/en-us/azure/azure-functions/functions-run-local)
+- [Visual Studio Code](https://code.visualstudio.com/)  
+ 
+### Visual Studio Code Extensions Required:
+
+- [Azurite](https://marketplace.visualstudio.com/items?itemName=Azurite.azurite)
+- [Azure CLI Extension](https://marketplace.visualstudio.com/items?itemName=ms-vscode.azurecli)
+- [Azure Account](https://marketplace.visualstudio.com/items?itemName=ms-vscode.azure-account)
+- [MS Dotnet Tools CSharp](https://marketplace.visualstudio.com/items?itemName=ms-dotnettools.csharp)
+- [Azure Developer CLI](https://marketplace.visualstudio.com/items?itemName=ms-azuretools.azure-dev)
+- [Azure Functions](https://marketplace.visualstudio.com/items?itemName=ms-azuretools.vscode-azurefunctions)
+- [Azure Logic Apps Standard Designer](https://marketplace.visualstudio.com/items?itemName=ms-azuretools.vscode-azurelogicapps)
+
+
+## Check Your Environment 
+PowerShell Script: checkenvironment.ps1
+
+The `checkenvironment.ps1` script is a PowerShell script located in the `scripts` folder. This script is used to check the environment setup for the labs.
+
+### Purpose
+
+The purpose of the `checkenvironment.ps1` script is to verify that all the necessary dependencies and configurations are in place for the integration environment lab. It performs various checks and validations to ensure that the environment is properly set up and ready for use.
+
+### Usage
+
+To use the `checkenvironment.ps1` script, follow these steps:
+
+1. Open a PowerShell terminal.
+2. Navigate to the `scripts` folder.
+3. Run the script using the following command:
+
+   ```powershell
+   .\checkenvironment.ps1
---
+++ scripts/checkenvironment.ps1
@@ -0,0 +1,152 @@
+$MissingSomething = [bool]::Parse("false")
+
+$greenCheck = @{
+    Object = [Char]8730
+    ForegroundColor = 'Green'
+    NoNewLine = $true
+    }
+
+# Check if the needed components are installed
+Write-Host "------------------------------------"
+Write-Host "Checking Azure CLI"
+try{ az --version }
+catch
+    { 
+        Write-Host ">>>> Azure CLI not installed.  Please install and try again" 
+        $MissingSomething = [bool]::Parse("true")
+    }
+Write-Host ""
+
+Write-Host "------------------------------------"
+Write-Host "Checking Azure Developer CLI (azd)"
+try{ 
+    $azdVersion = azd version
+    Write-Host @greenCheck
+    write-host " - Azure Developer CLI version: $azdVersion"
+}
+catch
+    { 
+        Write-Host ">>>> Azure Developer CLI not installed.  Please install and try again"  -ForegroundColor yellow
+        Write-Host ">>>> You can install it by running the following command in a terminal window: winget install microsoft.azd" -ForegroundColor yellow
+        $MissingSomething = [bool]::Parse("true")
+    }
+Write-Host ""
+
+Write-Host "------------------------------------"
+Write-Host "Checking Azure Functions Core Tools"
+try {
+    $funcVersion = func --version
+    Write-Host @greenCheck
+    Write-Host " - Azure Functions Core Tools version: $funcVersion"
+    }
+catch 
+    { 
+        Write-Host ">>>> Azure Functions Core Tools not installed.  Please install and try again" -ForegroundColor yellow
+        Write-Host ">>>> You can install it by running the visiting https://learn.microsoft.com/en-us/azure/azure-functions/functions-run-local" -ForegroundColor yellow
+        $MissingSomething = [bool]::Parse("true")
+    }
+Write-Host ""
+
+Write-Host "------------------------------------"
+Write-Host "Checking Visual Studio Code"
+try {
+    $funcVersion = code --version
+    Write-Host @greenCheck
+    Write-Host " - Visual Studio Code version: $funcVersion"
+    }
+catch 
+    { 
+        Write-Host ">>>> Visual Studio Code not installed.  Please install and try again" -ForegroundColor yellow
+        Write-Host ">>>> You can install it by visiting https://code.visualstudio.com/" -ForegroundColor yellow
+        $MissingSomething = [bool]::Parse("true")
+    }
+Write-Host ""
+
+# check if the needed vs code extensions are installed
+Write-Host "------------------------------------"
+Write-Host "Checking Visual Studio Code Extensions"
+
+$listofextensions= code --list-extensions
+
+if ($listofextensions -notcontains "azurite.azurite") {
+    Write-Host ">>>> Azurite.azurite extension not installed.  Please install and try again"
+    Write-Host ">>>> You can install it by running the following command in a terminal window:"
+    Write-Host ">>>> code --install-extension Azurite.azurite"
+    $MissingSomething = [bool]::Parse("true")
+} else {
+    Write-Host @greenCheck
+    Write-Host " - Azurite.azurite extension installed"
+}
+
+if ($listofextensions -notcontains "ms-vscode.azurecli") {
+    Write-Host ">>>> ms-vscode.azurecli extension not installed.  Please install and try again"
+    Write-Host ">>>> You can install it by running the following command in a terminal window:"
+    Write-Host ">>>> code --install-extension ms-vscode.azurecli"
+    $MissingSomething = [bool]::Parse("true")
+} else {
+    Write-Host @greenCheck
+    Write-Host " - ms-vscode.azurecli extension installed"
+}
+
+if ($listofextensions -notcontains "ms-vscode.azure-account") {
+    Write-Host ">>>> ms-vscode.azure-account extension not installed.  Please install and try again"
+    Write-Host ">>>> You can install it by running the following command in a terminal window:"
+    Write-Host ">>>> code --install-extension ms-vscode.azure-account"
+    $MissingSomething = [bool]::Parse("true")
+} else {
+    Write-Host @greenCheck
+    Write-Host " - ms-vscode.azure-account extension installed"
+}
+
+if ($listofextensions -notcontains "ms-dotnettools.csharp") {
+    Write-Host ">>>> ms-dotnettools.csharp extension not installed.  Please install and try again"
+    Write-Host ">>>> You can install it by running the following command in a terminal window:"
+    Write-Host ">>>> code --install-extension ms-dotnettools.csharp"
+    $MissingSomething = [bool]::Parse("true")
+} else {
+    Write-Host @greenCheck
+    Write-Host " - ms-dotnettools.csharp extension installed"
+}
+
+if ($listofextensions -notcontains "ms-azuretools.azure-dev") {
+    Write-Host ">>>> ms-azuretools.azure-dev extension not installed.  Please install and try again"
+    Write-Host ">>>> You can install it by running the following command in a terminal window:"
+    Write-Host ">>>> code --install-extension ms-azuretools.azure-dev"
+    $MissingSomething = [bool]::Parse("true")
+} else {
+    Write-Host @greenCheck
+    Write-Host " - ms-azuretools.azure-dev extension installed"
+}
+
+if ($listofextensions -notcontains "ms-azuretools.vscode-azurefunctions") {
+    Write-Host ">>>> ms-azuretools.vscode-azurefunctions extension not installed.  Please install and try again"
+    Write-Host ">>>> You can install it by running the following command in a terminal window:"
+    Write-Host ">>>> code --install-extension ms-azuretools.vscode-azurefunctions"
+    $MissingSomething = [bool]::Parse("true")
+} else {
+    Write-Host @greenCheck
+    Write-Host " - ms-azuretools.vscode-azurefunctions extension installed"
+}
+
+if ($listofextensions -notcontains "ms-azuretools.vscode-azurelogicapps") {
+    Write-Host ">>>> ms-azuretools.vscode-azurelogicapps extension not installed.  Please install and try again"
+    Write-Host ">>>> You can install it by running the following command in a terminal window:"
+    Write-Host ">>>> code --install-extension ms-azuretools.vscode-azurelogicapps"
+    $MissingSomething = [bool]::Parse("true")
+} else {
+    Write-Host @greenCheck
+    Write-Host " - ms-azuretools.vscode-azurelogicapps extension installed"
+}
+
+# Did we run into any problems?
+if ($MissingSomething)
+    {
+        Write-Host "------------------------------------"
+        Write-Host "One or more components are missing. " -ForegroundColor red
+        Write-Host "Please review the log above and install the missing components before continuing" -ForegroundColor red
+    }
+    else {
+        Write-Host "------------------------------------"
+        Write-Host "Everything looks good.  You are ready to go!" -ForegroundColor green
+        Write-Host "------------------------------------"
+    }
